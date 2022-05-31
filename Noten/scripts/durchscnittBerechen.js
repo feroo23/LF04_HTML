0cf412b79e58afb87cd4115a9b92395fc3db96ed
Ferhat44 <fgumus1234@gmail.com>
2022-05-31 08:49:59 +0200

    Durchschnittnoten

---
+++ Noten/scripts/durchscnittBerechen.js
@@ -0,0 +1,10 @@
+let durchscnittBerechen = () =>{
+    let note1 = parseInt(document.getElementById("note1"))
+    let note2 = parseInt(document.getElementById("note2"))
+    let note3 = parseInt(document.getElementById("note3"))
+    let note4 = parseInt(document.getElementById("note4"))
+    let erg = document.getElementById("erg");
+
+
+    erg = (note1 + note2 + note3 + note4) / 4
+}
